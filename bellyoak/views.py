b4987e6df26296fa5b89c3406a6fefb6c7b5199f
Martin de Spirlet <martindes01@gmail.com>
2019-09-06 18:00:13 +0100

    Map URLs and create templates

--- bellyoak/views.py
+++ bellyoak/views.py
@@ -1,3 +1,56 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+# Views
+
+def index(request):
+    context_dict = {
+        'message': "index",
+    }
+    return render(request, 'bellyoak/index.html', context_dict)
+
+def about(request):
+    context_dict = {
+        'message': "about",
+    }
+    return render(request, 'bellyoak/about.html', context_dict)
+
+def add_recipe(request):
+    context_dict = {
+        'title': "New recipe",
+        'message': "add_recipe",
+    }
+    return render(request, 'bellyoak/recipe.html', context_dict)
+
+def edit_recipe(request, id):
+    context_dict = {
+        'title': "Editing " + str(id),
+        'message': "edit_recipe",
+    }
+    return render(request, 'bellyoak/recipe.html', context_dict)
+
+def list_recipes(request):
+    context_dict = {
+        'message': "list_recipes",
+    }
+    return render(request, 'bellyoak/recipes.html', context_dict)
+
+def list_users(request):
+    context_dict = {
+        'message': "list_users",
+    }
+    return render(request, 'bellyoak/users.html', context_dict)
+
+def show_recipe(request, id):
+    context_dict = {
+        'title': str(id),
+        'message': "show_recipe",
+    }
+    return render(request, 'bellyoak/recipe.html', context_dict)
+
+def show_user(request, username):
+    context_dict = {
+        'title': username,
+        'message': "show_user",
+    }
+    return render(request, 'bellyoak/user.html', context_dict)
