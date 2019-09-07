f0b5f49af6525bdd6707020464b3287cc7997c22
martindes01 <martindes01@gmail.com>
2019-09-07 20:57:16 +0100

    Create population script and configure admin interface

--- bellyoak/admin.py
+++ bellyoak/admin.py
@@ -2,6 +2,35 @@ from django.contrib import admin
 
 from bellyoak.models import Ingredient, Instruction, Recipe
 
-admin.site.register(Ingredient)
-admin.site.register(Instruction)
-admin.site.register(Recipe)
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = (
+        'recipe',
+        'name',
+        'quantity',
+        'unit',
+    )
+
+@admin.register(Instruction)
+class InstructionAdmin(admin.ModelAdmin):
+    list_display = (
+        'recipe',
+        'index',
+        'text',
+    )
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'title',
+        'servings',
+        'diet',
+        'views',
+        'likes',
+    )
+    prepopulated_fields = {
+        'slug': (
+            'title',
+        ),
+    }
